7a43130f90b9213cd8ccbe583033fab45c0a942d
Li Yu <ly257@oath.com>
2018-01-27 18:27:34 -0800

    framework init

---
+++ index.js
@@ -0,0 +1,33 @@
+const expressPort = process.env.PORT || '3000';
+const debug = require('debug')('Map:Server');
+const env = process.env.NODE_ENV;
+
+try {
+    const memwatch = require('memwatch-next');
+
+    memwatch.on('leak', info => {
+        console.error('Map::Server:Memory leak detected:\n', info);
+    });
+    memwatch.on('stats', stats => {
+        console.error('Map::Server:Stats:\n', stats);
+    });
+}  catch (e) {
+    console.log('Map::Error', e);
+}
+
+let app;
+if (env === 'dev') {
+    /* Attach Marko Just In Time compiling. */
+    require('marko/node-require').install();
+    app = require('./server/express');
+} else {
+    app = require('./serverDist/express');
+}
+
+app.listen(expressPort, (error) => {
+    if (error) {
+        debug('Unable to listen.', error);
+        return;
+    }
+    debug('Map server started on port ' + expressPort);
+});
---
+++ server/express.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const compress = require('compression');
+const express = require('express');
+const Promise = require('bluebird');
+const httpStatus = require('http-status');
+const routes = require('./routes');
+const path = require('path');
+
+Promise.config({
+    cancellation: true
+});
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use('/', routes);
+app.use('/static', express.static(path.join(__dirname, '../dist')));
+app.use('/health-check', (req, res) => res.json({
+    status: 'success'
+}));
+
+module.exports = app;
